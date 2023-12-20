5386d2aa68945cc427d74c58594a44a594e227f7
RJ025 <ritikjain025@gmail.com>
2023-12-20 19:30:52 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // println!("the secret number is {secret_number}");
+
+    loop {
+        println!("Please enter the number");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+        
+        let guess : u32 = match guess.trim().parse() {
+            Ok(num) => num ,
+            Err(_) => continue,
+        };
+
+        println!("you guessed : {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            }
+        }
+    }
+}
